bd3599172307c0d9f22b2898d515a83b92a56f00
tophsm <67989403+tophsm@users.noreply.github.com>
2025-04-19 20:41:07 -0700

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,24 +1,22 @@
-#Project 4
 import random
 
 def play():
     user = input("Choose: Rock (R), Paper (P), or Scissors(S)? ")
     computer = random.choice(["R", "P", "S"]) 
 
-    if user == computer:
-        return "It's a tie."
+    if user.upper() == computer:
+        return f"We both chose {computer}! It's a tie."
 
-    if is_win(user, computer): #user is player and computer is opponent
-        return "You won!"
+    if is_win(user, computer): #user is player vs computer
+        return f"You won! {user.upper()} beats {computer}!"
     
-    return "Sorry! You lost." # do not need if statement before this. returns only if computer won.
+    return f"Sorry! {computer} beats {user.upper()}. You lost." # do not need if statement before this. returns only if computer won.
 
 #Rules for winning: R > S, P > R, S > P
 
-def is_win(player, opponent):
+def is_win(player, computer):
     #return true if player wins
-    if (player == "R" and opponent == "S") or (player == "P" and opponent == "R") or \
-        (player == "S" and opponent == "P"):
+    if (player.upper() == "R" and computer == "S") or (player.upper() == "P" and computer == "R") or (player.upper() == "S" and computer == "P"):
         return True
 
 print(play())
